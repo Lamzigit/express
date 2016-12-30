ba4314e234a047b27d8bbbf0e121466491d1bc71
林志杰 <18814103637@163.com>
2016-12-30 09:12:02 +0800

    ManagerDao接口实现通过测试

--- src/main/com/express/dao/daoImpl/ManagerDaoImpl.java
+++ src/main/com/express/dao/daoImpl/ManagerDaoImpl.java
@@ -4,9 +4,13 @@ import main.com.express.dao.ManagerDao;
 import main.com.express.entity.Manager;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.criterion.Restrictions;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -14,7 +18,7 @@ import java.util.List;
 /**
  * Created by linzhijie on 2016/12/29.
  */
-@Transactional
+
 @Repository
 public class ManagerDaoImpl implements ManagerDao{
 
@@ -27,35 +31,46 @@ public class ManagerDaoImpl implements ManagerDao{
 
     @Override
     public Manager getManagerById(int id) {
-        //Session session = sessionFactory.getCurrentSession();
-        Session session = sessionFactory.openSession();
+        Session session = sessionFactory.getCurrentSession();
+        //Session session = sessionFactory.openSession();
         return (Manager)session.createCriteria(Manager.class).add(Restrictions.eq("id",id)).uniqueResult();
     }
 
     @Override
     public List<Manager> getAllManager() {
-        //Session session = sessionFactory.getCurrentSession();
-        Session session = sessionFactory.openSession();
+        Session session = sessionFactory.getCurrentSession();
+        //Session session = sessionFactory.openSession();
         return (List<Manager>)session.createCriteria(Manager.class).list();
     }
 
     @Override
     public boolean addManager(Manager man) {
-        Session session = sessionFactory.openSession();
+        Session session = sessionFactory.getCurrentSession();
+        //Session session = sessionFactory.openSession();
         return (session.save(man)!=null);
     }
 
     @Override
+    //@Transactional("transactionManager")
     public boolean delManager(int id) {
-        Session session = sessionFactory.openSession();
-        session.delete(getManagerById(id));
-        return true;
+        Session session = sessionFactory.getCurrentSession();
+        //Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        String hql = "delete from Manager m where m.id = :id";
+        Query query = session.createQuery(hql);
+        query.setParameter("id",id);
+        int i = query.executeUpdate();
+        transaction.commit();
+        return i>0;
     }
 
     @Override
     public boolean updManager(Manager man) {
-        Session session = sessionFactory.openSession();
+        Session session = sessionFactory.getCurrentSession();
+        //Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
         session.merge(man);
+        transaction.commit();
         return true;
     }
 }
