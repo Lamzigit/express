e5ba545e0f7a11648079f69af99c33aa382028ee
林志杰 <18814103637@163.com>
2017-01-02 16:05:49 +0800

    添加angular指令

--- web/WEB-INF/view/js/neworder.js
+++ web/WEB-INF/view/js/neworder.js
@@ -1,8 +1,8 @@
 /**
  * Created by linzhijie on 2017/1/1.
  */
-var app = angular.module("orderAdd",[]);
-app.controller("orderController",function ($scope,$http) {
+var appAdd = angular.module("orderAdd",[]);
+appAdd.controller("addController",function ($scope,$http) {
     $scope.countPrice = function () {
         if($scope.weight>10&&$scope.weight<50)
             $scope.price = 10 + ($scope.weight-2)*1.3;//1kg起步,底价10元，超出部分按1.3倍叠加
@@ -11,9 +11,9 @@ app.controller("orderController",function ($scope,$http) {
         else
             $scope.price = 10;
     }
-    
+
     $scope.postOrder = function () {
-        $http({
+     $http({
             url : 'http://localhost:8080/express/order/add',
             method : "post",
             params : {
@@ -24,6 +24,29 @@ app.controller("orderController",function ($scope,$http) {
                 price : $scope.price,
                 state : $scope.state,
             }
+
         })
+        /*console.log(
+            "phone " + $scope.phone + "\n" +
+            "saddress " + $scope.saddress + "\n" +
+            "raddress " + $scope.raddress + "\n" +
+            "weight " + $scope.weight + "\n" +
+            "price " + $scope.price + "\n" +
+            "uid " + $scope.uid + "\n" +
+            "state " + $scope.state);*/
+
+    }
+});
+
+var appList = angular.module("orderList",[]);
+appList.controller("listController",function ($scope,$http) {
+    $scope.getOrderList= function () {
+        $http({
+            url : 'http://localhost:8080/express/order/list',
+            methode : post,
+            params : {
+                phone : $scope.phone
+            }
+        });
     }
 });
