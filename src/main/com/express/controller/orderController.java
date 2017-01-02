7d3e8bd8b7fd1b57cbd96cff2a94599c87091f74
林志杰 <18814103637@163.com>
2017-01-02 16:04:59 +0800

    增加添加订单逻辑，

--- src/main/com/express/controller/orderController.java
+++ src/main/com/express/controller/orderController.java
@@ -1,11 +1,20 @@
 package main.com.express.controller;
 
+import main.com.express.entity.Address;
 import main.com.express.entity.Orders;
+import main.com.express.entity.User;
+import main.com.express.service.AddressService;
 import main.com.express.service.OrdersService;
 import main.com.express.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
 /**
  * Created by linzhijie on 2017/1/1.
@@ -19,12 +28,41 @@ public class orderController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private AddressService addressService;
+
     @RequestMapping(value = "/add")
     public String addOrder(Orders orders){
-        orders.setUid(userService.getUserByPhone(orders.getPhone()).getId());
-        orders.setTransnum(String.valueOf((1+Math.random()*(100000000))));
+        String phone = orders.getPhone();
+        //前端页面无法获取并传送用户id,唯有重新查询用户信息
+        User user = userService.getUserByPhone(phone);
+        orders.setUid(user.getId());
+        //生成随机数作为单号
+        orders.setTransnum(String.valueOf((100000+Math.random()*(999999-100000+1))));//随机数范围：(最小值+Math.random()*(最大值-最小值+1))
+        //添加订单
         ordersService.addOrder(orders);
+        //获取用户的历史地址，
+        Address address = addressService.getAddressByPhone(orders.getPhone());
+        //若老用户更改地址，则更新地址记录
+        if(address != null && (!orders.getRaddress().equals(address.getRaddress()) || !orders.getSaddress().equals(address.getSaddress()))){
+            System.out.print("update");
+            addressService.updAddress(address);
+        }
+        //若是新用户，则添加新地址
+        else {
+            //封装地址
+            address = new Address();
+            address.setRaddress(orders.getRaddress());
+            address.setSaddress(orders.getSaddress());
+            address.setPhone(orders.getPhone());
+            addressService.addAddress(address);
+        }
         return "orderhistory";
     }
 
+    @RequestMapping(value = "/list")
+    public @ResponseBody List<Orders> listOrder(String phone){
+        List<Orders> orderses = ordersService.getOrderByphone(phone);
+        return orderses;
+    }
 }
